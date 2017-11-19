297f656d07fe56a3ccd16f35b7c366c729b7b62b
Andy Pitcher <162066@supinfo.com>
2017-11-19 16:21:09 -0500

    1.2

--- android_syscalls.py
+++ android_syscalls.py
@@ -1,49 +1,51 @@
 #!/usr/bin/env python
 #
 #Andy Pitcher <andy.pitcher@concordia.ca>
+#This program trains and collects the system calls run by a given app by generating random usage
 #
+##############################################################################################
 
 import subprocess
 import pexpect
 import sys,os
+from random import randint
 
-#adb="/usr/share/platform-tools/adb"
-
-temp_strace_dir="/sdcard/Download/strace/"
+#Env setup
+attempt=randint(0,100)
+temp_strace_dir="/sdcard/Download/strace/"+str(attempt)+"/"
 app="com.android.calculator2"
+rand_iterations="200"
 
+#Create training directory on Android device
+os.system("adb shell mkdir "+temp_strace_dir)  
+
+#Retrieve zygote PID to attach the future app pid
 cmd_get_zygote_pid="adb shell ps | grep zygote | awk '{print $2}'"
 zygote_PID=subprocess.check_output([cmd_get_zygote_pid], shell=True)
-cmd_get_all_pid="adb shell pgrep -P"+zygote_PID
-cmd_get_all_strace="adb shell strace -ff -o"+temp_strace_dir+"output.txt -p"+str(zygote_PID)
-cmd_get_app_pid="adb shell ps | grep "+app+" | awk '{print $2}'"
-subprocess.Popen([cmd_get_all_strace], shell=True)
-zygote_child_PID=subprocess.check_output([cmd_get_all_pid], shell=True)
 
+#Get all attached Zigote's child process and App PID to pull the its strace output
+cmd_get_all_pid="adb shell pgrep -P"+zygote_PID
+cmd_get_app_pid="adb shell ps | grep "+app+" | awk '{print $2}'"
+
+#Start zygote's PID strace subprocess in bg which collects and copy its children process in temp_strace_dir 
+cmd_get_all_strace="adb shell strace -ff -o"+temp_strace_dir+"trace -p"+str(zygote_PID)
+proc_strace=subprocess.Popen([cmd_get_all_strace], shell=True)
+
+#Print all attached Zygote's child process before lauching the app
+zygote_child_PID=subprocess.check_output([cmd_get_all_pid], shell=True)
 print "Number of child process\n"+zygote_child_PID
 
-raw_input("Press any key to start the next? Default["+app+"]")
-os.system("adb shell monkey -p "+app+" -c android.intent.category.LAUNCHER 1")
+#input("Press any key to start the next? Default["+app+"]")
 
+#Start the app and launch random unit tests: rand_iterations, throttle can be removed or increased to reduce the delay between events
+os.system("adb shell monkey --throttle 50 -p "+app+" -c android.intent.category.LAUNCHER "+rand_iterations)
+app_PID=subprocess.check_output([cmd_get_app_pid], shell=True)
 
+#Stop the app
+os.system("adb shell am force-stop "+app)
+#Stop the Zigote strace
+proc_strace.kill()
 
-
-
-
-
-#command="adb shell strace -o "+ temp_strace_dir+" -ff -p"+str(i)
-#for i in zygote_PID:
-#	os.system("adb shell strace -o "+ temp_strace_dir+"-ff -p"+str(i)+" &")
-#subprocess.Popen(command,shell=True)
-#adb_android.shell("monkey -p com.android.calculator2 -c android.intent.category.LAUNCHER 1")
-
-
-
-#syscalls = set(ln.split('(', 1)[0] for ln in strace_output)
-#for i in syscalls:
-#	print i
-#child = pexpect.spawn(adb + " shell")
-#child.logfile_send = sys.stdout
-#child.sendline("ls")
-#child.expect(pexpect.EOF)
-#print child.read()
+#Pull the trace.app_PID file to the computer and remove temp_strace_dir
+os.system("adb pull "+temp_strace_dir+"trace."+app_PID)
+os.system("adb shell rmdir -r "+temp_strace_dir)
